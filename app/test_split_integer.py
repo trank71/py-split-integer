d2a6fdbc14b752468a8d240debe516a20f11dfc4
Ambra <trank13f@gmail.com>
2026-01-18 13:52:43 +0100

    realise test test_should_add_zeros_when_is_less_than_number_of_parts and optimise test_should_return_part_equals_to_value_when_split_into_one_part test

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -18,9 +18,8 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 8
     number_of_parts = 1
-    result = [8]
     parts = split_integer(value, number_of_parts)
-    assert parts == result
+    assert parts == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -31,4 +30,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    value = 3
+    number_of_parts = 6
+    parts = split_integer(value, number_of_parts)
+    assert parts.count(0) == number_of_parts - value
