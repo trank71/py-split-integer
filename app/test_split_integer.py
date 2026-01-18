15938d711ea10e72f9cf5f3197d359293bac433d
Ambra <trank13f@gmail.com>
2026-01-18 13:35:29 +0100

    realise test test_should_be_sorted_when_they_are_not_equal and fix test before

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,13 +2,16 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    parts = [4, 4, 4, 5]
     value = 17
+    number_of_parts = 4
+    parts = split_integer(value, number_of_parts)
     assert sum(parts) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    parts = [4, 4, 4, 5]
+    value = 17
+    number_of_parts = 4
+    parts = split_integer(value, number_of_parts)
     assert max(parts) - min(parts) <= 1
 
 
@@ -16,12 +19,15 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     value = 8
     number_of_parts = 1
     result = [8]
-    func = split_integer(value, number_of_parts)
-    assert result == func
+    parts = split_integer(value, number_of_parts)
+    assert parts == result
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    value = 17
+    number_of_parts = 4
+    parts = split_integer(value, number_of_parts)
+    assert sorted(parts) == parts
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
