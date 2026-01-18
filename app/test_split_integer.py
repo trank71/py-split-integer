2fe68907953945d8e8365b2ab36a917a5fc0500a
Ambra <trank13f@gmail.com>
2026-01-18 14:16:09 +0100

    fix test_should_split_into_equal_parts_when_value_divisible_by_parts and use set instead of min/max func

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -12,7 +12,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     value = 17
     number_of_parts = 4
     parts = split_integer(value, number_of_parts)
-    assert max(parts) - min(parts) <= 1
+    assert len(set(parts)) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
